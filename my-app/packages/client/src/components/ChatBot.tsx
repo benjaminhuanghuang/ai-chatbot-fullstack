6bb0bd71e68299a30820ecaa04486d37aae127fc
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-12-11 00:32:43 -0800

    client

--- my-app/packages/client/src/components/ChatBot.tsx
+++ my-app/packages/client/src/components/ChatBot.tsx
@@ -1,33 +1,53 @@
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useRef } from 'react';
 
 type FormData = {
    prompt: string;
 };
 
 export const ChatBot = () => {
-   const { register, handleSubmit, reset } = useForm<FormData>();
+   const conversationId = useRef(crypto.randomUUID());
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
-   const onSubmit = (data: FormData) => {
-      console.log(data);
+   const onSubmit = ({ prompt }: FormData) => {
+      // console.log(data);
       reset();
+      axios
+         .post('/api/chatbot', {
+            prompt,
+            conversationId: conversationId.current,
+         })
+         .then((response) => {
+            console.log(response.data);
+         });
+   };
+
+   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault();
+         handleSubmit(onSubmit)();
+      }
    };
 
    return (
       <form
          onSubmit={handleSubmit(onSubmit)}
+         onKeyDown={onKeyDown}
          className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
       >
          <textarea
             {...register('prompt', {
                required: true,
+               validate: (value) => value.trim().length > 0,
             })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask anything"
             maxLength={1000}
          />
-         <Button className="rounded-full w-9 h-9">
+         <Button className="rounded-full w-9 h-9" disabled={!formState.isValid}>
             <FaArrowUp />
          </Button>
       </form>
